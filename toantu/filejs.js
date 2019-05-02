e44eed039e6c2ca8329857a6f89b7e3ce5cac331
naturevn <you@example.com>
2019-05-02 14:15:07 +0700

    Update Js

---
+++ toantu/filejs.js
@@ -0,0 +1,9 @@
+
+		let inputWidth;
+		let inputHeight;
+		inputWidth = prompt(" Nhập inputWidth");
+		inputHeight = prompt(" Nhập inputHeight");
+		let width = parseInt(inputWidth);
+		let height = parseInt(inputHeight);
+		let area = width * height;
+		document.write("The area is: " + area);
