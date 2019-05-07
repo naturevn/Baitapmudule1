37ad740490d5f7927446588ebbc526b4f48609c7
naturevn <you@example.com>
2019-05-07 08:49:02 +0700

    update file js

--- tinhsongay/file.js
+++ tinhsongay/file.js
@@ -2,7 +2,7 @@ function songay() {
 	let day = document.getElementById('day').value;
 	switch (day) {
 		case '2':
-			document.getElementById('hienngay').innerHTML='Tháng có 28 hoặc 29 ngày';
+			document.getElementById('hienngay').innerHTML='Tháng ' + day + ' có 28 hoặc 29 ngày';
 			break;
 		case '1':
 		case '3':
@@ -11,16 +11,16 @@ function songay() {
 		case '8':
 		case '10':
 		case '12':
-			document.getElementById('hienngay').innerHTML='Tháng có 31 ngày';
+			document.getElementById('hienngay').innerHTML='Tháng ' + day + ' có 31 ngày';
 			break;
 		case '4':
 		case '6':
 		case '9':
 		case '11':
-			document.getElementById('hienngay').innerHTML='Tháng có 30 ngày';
+			document.getElementById('hienngay').innerHTML='Tháng ' + day + ' có 30 ngày';
 			break;
 		default:
-			document.getElementById('hienngay').innerHTML='Tháng không tồn tại';
+			document.getElementById('hienngay').innerHTML='Tháng ' + day + ' không tồn tại';
 			break;
 		}
 	event.preventDefault();
