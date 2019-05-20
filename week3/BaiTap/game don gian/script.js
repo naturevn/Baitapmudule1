9f4d470e1161231cc49c537cd11f549527cf330f
naturevn <you@example.com>
2019-05-20 16:36:38 +0700

    update game don gian

--- week3/BaiTap/game don gian/script.js
+++ week3/BaiTap/game don gian/script.js
@@ -1,35 +1,35 @@
-/**
- * Created by nhatnk on 4/26/17.
- */
-
-function Hero(image, top, left, size) {
+function Hero(image, top, right, size) {
     this.image = image;
     this.top = top;
-    this.left = left;
+    this.right = right;
     this.size = size;
 
     this.getHeroElement = function() {
         return '<img width="' + this.size + '"' +
             ' height="' + this.size + '"' +
             ' src="' + this.image + '"' +
-            ' style="top: ' + this.top + 'px; left:' + this.left + 'px;position:absolute;" />';
+            ' style="top: ' + this.top + 'px; right:' + this.right + 'px;position:absolute;" />';
     }
 
     this.moveRight = function() {
-        this.left += 60;
-        console.log('ok: ' + this.left);
+        this.right += 1;
+        console.log('ok: ' + this.right);
     }
 
 }
 
-var hero = new Hero('car.gif', 20, 30, 200);
+
+var hero = new Hero('cargif.gif', 20, 30, 500);
 
 function start() {
-    if (hero.left < window.innerWidth - hero.size) {
+    if (hero.right < window.innerWidth - hero.size) {
         hero.moveRight();
+
+        document.getElementById('game').innerHTML = hero.getHeroElement();
+        setTimeout(start, 10)
+    } else {
+
     }
-    document.getElementById('game').innerHTML = hero.getHeroElement();
-    setTimeout(start, 500)
 }
 
 start();
